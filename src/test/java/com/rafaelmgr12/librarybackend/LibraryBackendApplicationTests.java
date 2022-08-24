83a0e4950f3da1efc4278f39a21ffb99f3f4738f
Rafael Ribeiro <ribeirorafaelmatheus@gmail.com>
2022-08-24 14:59:49 -0300

    start the java project

---
+++ src/test/java/com/rafaelmgr12/librarybackend/LibraryBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafaelmgr12.librarybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
